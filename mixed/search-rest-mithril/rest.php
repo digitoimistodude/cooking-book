d7298fc858d56d2bcac59a6c48e5616ab2985b80
Timi Wahalahti <timi@wahalahti.fi>
2019-10-23 12:07:10 +0300

    update rest search code

--- mixed/search-rest-mithril/rest.php
+++ mixed/search-rest-mithril/rest.php
@@ -3,16 +3,19 @@
  * @Author: Timi Wahalahti
  * @Date:   2019-10-22 16:02:40
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-10-22 16:05:46
+ * @Last Modified time: 2019-10-23 12:06:51
  */
 
 // Add our REST API endpoint for search.
 add_action( 'rest_api_init', 'wpfi_rest_api_search_init' );
 function wpfi_rest_api_search_init() {
-  register_rest_route( 'wpfi/v1', '/search', array(
-    'methods'   => 'GET',
-    'callback'  => 'wpfi_rest_api_search',
-  ) );
+  register_rest_route( 'wpfi/v1',
+    '/search',
+      array(
+      'methods'   => 'GET',
+      'callback'  => 'wpfi_rest_api_search',
+    )
+  );
 } // end wpfi_rest_api_search_init
 
 // REST API search endpoint callback.
@@ -29,7 +32,9 @@ function wpfi_rest_api_search( $request ) {
 
   // do search query
   $args = array(
-    's'                       => $_GET['s'],
+    's'                       => sanitize_text_field( $_GET['s'] ),
+    'posts_status'            => 'publish',
+    'has_password'            => false,
     'no_found_rows'           => true,
     'cache_results'           => true,
     'update_post_term_cache'  => false,
@@ -45,6 +50,17 @@ function wpfi_rest_api_search( $request ) {
       // add result to response
       $item = $rest_controller->prepare_response_for_collection( $query->post );
       $item->link = get_the_permalink( $item->ID );
+
+      // remove some un-necssary data
+      unset( $item->post_content );
+      unset( $item->post_password );
+      unset( $item->to_ping );
+      unset( $item->pinged );
+      unset( $item->post_content_filtered );
+      unset( $item->post_mime_type );
+      unset( $item->filter );
+      unset( $item->menu_order );
+
       $data[] = $item;
     }
   }
--- mixed/search-rest-vue/rest.php
+++ mixed/search-rest-vue/rest.php
@@ -1,40 +1,69 @@
 <?php
+/**
+ * @Author: Timi Wahalahti
+ * @Date:   2019-10-22 16:02:40
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-10-23 12:06:57
+ */
 
-function wpfi_rest_api_search_init() {
-  register_rest_route( 'wpfi/v1', '/search', array(
-    'methods'		=> 'GET',
-    'callback'	=> 'wpfi_rest_api_search',
-  ) );
-}
+// Add our REST API endpoint for search.
 add_action( 'rest_api_init', 'wpfi_rest_api_search_init' );
+function wpfi_rest_api_search_init() {
+  register_rest_route( 'wpfi/v1',
+    '/search',
+      array(
+      'methods'   => 'GET',
+      'callback'  => 'wpfi_rest_api_search',
+    )
+  );
+} // end wpfi_rest_api_search_init
 
+// REST API search endpoint callback.
 function wpfi_rest_api_search( $request ) {
-	$data = array();
+  $data = array();
 
-	if ( ! isset( $_GET['s'] ) ) {
-		return $data;
-	}
+  // bail if no search param.
+  if ( ! isset( $_GET['s'] ) ) {
+    return $data;
+  }
 
-	$rest_controller = new WP_REST_Post_Types_Controller();
+  // used later to prepare post object
+  $rest_controller = new WP_REST_Post_Types_Controller();
 
-	$args = array(
-		's'   				=> $_GET['s'],
-		'no_found_rows'          	=> true,
-		'cache_results'          	=> true,
-		'update_post_term_cache' 	=> false,
-	);
+  // do search query
+  $args = array(
+    's'                       => sanitize_text_field( $_GET['s'] ),
+    'posts_status'            => 'publish',
+    'has_password'            => false,
+    'no_found_rows'           => true,
+    'cache_results'           => true,
+    'update_post_term_cache'  => false,
+  );
 
   $query = new WP_Query( $args );
 
+  // loop results
   if ( $query->have_posts() ) {
-  	while ( $query->have_posts() ) {
-  		$query->the_post();
+    while ( $query->have_posts() ) {
+      $query->the_post();
 
-      		$item = $rest_controller->prepare_response_for_collection( $query->post );
-      		$item->link = get_the_permalink( $item->ID );
-      		$data[] = $item;
-  	}
+      // add result to response
+      $item = $rest_controller->prepare_response_for_collection( $query->post );
+      $item->link = get_the_permalink( $item->ID );
+
+      // remove some un-necssary data
+      unset( $item->post_content );
+      unset( $item->post_password );
+      unset( $item->to_ping );
+      unset( $item->pinged );
+      unset( $item->post_content_filtered );
+      unset( $item->post_mime_type );
+      unset( $item->filter );
+      unset( $item->menu_order );
+
+      $data[] = $item;
+    }
   }
 
   return $data;
-}
+} // end wpfi_rest_api_search
