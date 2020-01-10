83d2e669b99563a655252ea1bee445186a01e3eb
Roni Laukkarinen <roni@dude.fi>
2020-01-10 15:02:21 +0200

    Change to generally needed optons page

--- php/acf-options-page.php
+++ php/acf-options-page.php
@@ -4,16 +4,15 @@
  *
  * @link https://www.advancedcustomfields.com/resources/acf_add_options_page/
  */
-
 if ( function_exists( 'acf_add_options_page' ) ) {
   $option_page = acf_add_options_page(array(
-    'page_title'  => 'Alatunniste',
-    'menu_title'  => 'Alatunniste',
-    'menu_slug'   => 'footer-settings',
+    'page_title'  => 'Globaalit sivuston laajuiset kentät ja asetukset',
+    'menu_title'  => 'Globaalit',
+    'menu_slug'   => 'global-settings',
     'capability'  => 'edit_posts',
     'redirect'    => false,
     'position'    => 30,
     // Better icons: https://icongr.am/
-    'icon_url'    => 'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" fill="currentColor" width="20" height="20" viewBox="0 0 24 24"><path d="M6,2H18C19.1,2 20,2.9 20,4V20C20,21.1 19.1,22 18,22H6C4.9,22 4,21.1 4,20V4C4,2.9 4.9,2 6,2M6,16V20H18V16H6Z"/></svg>' ),
+    'icon_url'    => 'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="mdi-view-grid-plus-outline" width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M3 21H11V13H3M5 15H9V19H5M3 11H11V3H3M5 5H9V9H5M13 3V11H21V3M19 9H15V5H19M18 16H21V18H18V21H16V18H13V16H16V13H18Z"/></svg>' ),
   ));
 }
