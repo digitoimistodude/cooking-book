7fccf053306aca2bac69f51577666a24956f6f8d
Roni Laukkarinen <roni@dude.fi>
2020-03-06 11:49:00 +0200

    Escapes and formatting

--- php/acf-repeater.php
+++ php/acf-repeater.php
@@ -1,15 +1,16 @@
 <?php if ( have_rows( 'repeater' ) ) :
-  while( have_rows( 'repeater' ) ) : the_row();
+  while ( have_rows( 'repeater' ) ) : the_row();
 
-    // Fields
+        // Fields
     $repeater_image = get_sub_field( 'repeater_image' );
     $repeater_title = get_sub_field( 'repeater_title' )
 
     if ( ! empty( $repeater_title ) ) : ?>
-      <h2><?php echo $repeater_title; ?></h2>
+      <h2><?php echo esc_attr( $repeater_title ); ?></h2>
     <?php endif;
     if ( ! empty( $repeater_image ) ) : ?>
-      <img src="<?php echo $repeater_image['sizes'][ 'large' ]; ?>" alt="<?php echo $repeater_title; ?>" />
+      <img src="<?php echo esc_url( $repeater_image['sizes']['large'] ); ?>" alt="<?php echo esc_attr( $repeater_title ); ?>" />
     <?php endif;
   endwhile;
 endif;
+?>
